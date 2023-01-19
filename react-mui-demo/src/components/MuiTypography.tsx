5a11ddc8ba45bef6b35d4f070422e9fbc3c25248
Sneha Poulose <snehapoulose1999@gmail.com>
2023-01-19 09:55:54 +0530

    -added typography material ui to the react application.

---
+++ react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,18 @@
+import Typography from '@mui/material/Typography';
+
+export const MuiTypography = () => {
+  return (
+    <div>
+  <Typography variant='h1'> h1 Heading</Typography>
+  <Typography variant='h2'> h2 Heading</Typography>
+  <Typography variant='h3'> h3 Heading</Typography>
+  <Typography variant='h4' component='h1' gutterBottom> h4 Heading</Typography>
+  <Typography variant='h5' gutterBottom> h5 Heading</Typography>
+  <Typography variant='h6' gutterBottom> h6 Heading</Typography>
+  <Typography variant='subtitle1'> SubTitle 1</Typography>
+  <Typography variant='subtitle2'> SubTitle 2</Typography>
+  <Typography variant='body1'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur pariatur eveniet unde nemo recusandae nostrum temporibus numquam aliquid, error dolore aut ipsa ipsum ad! Inventore itaque nobis cum fugiat reiciendis.</Typography>
+  <Typography variant='body2'> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus, quis aspernatur provident, error nostrum ullam praesentium temporibus illum assumenda maiores laborum quisquam, quod eos ratione a placeat. Ipsum, labore odit.</Typography>
+  </div>
+  )
+}
